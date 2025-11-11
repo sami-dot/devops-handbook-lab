0765b3a47bff4822bd5b1cef508034256d2f2d60
sami-dot <79455787+sami-dot@users.noreply.github.com>
2025-11-11 07:20:39 +0000

    updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from DevOps Handbook! 🚀');
+  res.send('Hi!');
 });
 
 app.listen(3000, () => console.log('Running on :3000'));
