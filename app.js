739ab7533cb094a24c17462c466132b9f7ae4e28
sami-dot <samiseymen1010@gmail.com>
2025-11-11 09:24:22 +0300

    iniial files

---
+++ app.js
@@ -0,0 +1,10 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from DevOps Handbook! 🚀');
+});
+
+app.listen(3000, () => console.log('Running on :3000'));
+module.exports = app;
